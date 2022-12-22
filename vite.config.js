bbb7457160a6f2b99ae752adada1af13bd7fd599
de-oz <101826623+de-oz@users.noreply.github.com>
2022-12-22 22:18:31 +0300

    chore: integrate and set up Quasar

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+
+    quasar({
+      sassVariables: 'src/assets/quasar-variables.scss',
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
